37d5b974f6292d94b8cd3932a82006daa0369553
Md. Thoukir Hasan Ha-Mim <ha-mim88@outlook.com>
2025-11-18 16:06:46 +0600

    course crud

---
+++ webapp/Pages/Admin/CourseMgt/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using webapp.Data;
+using webapp.Entities;
+
+namespace webapp.Pages.Admin.CourseMgt
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly webapp.Data.ApplicationDbContext _context;
+
+        public DeleteModel(webapp.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Course Course { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Course.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (course is not null)
+            {
+                Course = course;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Course.FindAsync(id);
+            if (course != null)
+            {
+                Course = course;
+                _context.Course.Remove(Course);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ webapp/Pages/Admin/CourseMgt/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using webapp.Data;
+using webapp.Entities;
+
+namespace webapp.Pages.Admin.CourseMgt
+{
+    public class IndexModel : PageModel
+    {
+        private readonly webapp.Data.ApplicationDbContext _context;
+
+        public IndexModel(webapp.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Course { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Course = await _context.Course.ToListAsync();
+        }
+    }
+}
