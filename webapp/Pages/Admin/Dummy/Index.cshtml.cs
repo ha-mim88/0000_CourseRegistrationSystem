3786bbb696100bd48434ed28d7bc921e2d2c2934
Md. Thoukir Hasan Ha-Mim <ha-mim88@outlook.com>
2025-11-18 15:19:03 +0600

    Add project files.

---
+++ webapp/Pages/Admin/Dummy/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webapp.Pages.Admin.Dummy
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
